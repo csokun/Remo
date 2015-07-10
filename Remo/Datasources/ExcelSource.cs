25424addb2dc8972cabde2d73525f439ed41e119
Chorn Sokun <chornsokun@gmail.com>
2015-07-11 02:28:39 +1000

    Reading test case from excel sheet & execute some basic scenario

---
+++ Remo/Datasources/ExcelSource.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+using Remo.Descriptor;
+
+namespace Remo.Datasources
+{
+	public class ExcelSource : IDatasource, IDisposable
+	{
+		private const int TestStepStartAt = 2;
+
+		private ExcelPackage excelPackage;
+
+		public ExcelSource(string filename)
+		{
+			var file = new FileInfo(filename);
+			if (!file.Exists)
+				throw new IOException(filename + " does not exist.");
+
+			excelPackage = new ExcelPackage(file);
+		}
+
+		public TestCase Get(string testId)
+		{
+			var ws = excelPackage.Workbook.Worksheets[testId];
+			try
+			{
+				TestCase ut = new TestCase();
+				
+				int row = TestStepStartAt;
+				while (!string.IsNullOrWhiteSpace(ws.Cells["B" + row].GetValue<string>()))
+				{
+					var step = GetStepAt(row, ws);
+					ut.Add(step);
+					row ++;
+				}
+
+				return ut;
+			}
+			catch (Exception e)
+			{
+				return null;
+			}
+		}
+
+		public void Report(TestCase unit)
+		{
+			throw new NotImplementedException();
+		}
+
+		#region Dispose Pattern https://msdn.microsoft.com/en-us/library/b1yfkh5e(v=vs.110).aspx
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing) return;
+			if (excelPackage != null) excelPackage.Dispose();
+		}
+
+		#endregion
+
+		private TestStep GetStepAt(int row, ExcelWorksheet sheet)
+		{
+			var testStep = new TestStep
+			{
+				Action = sheet.Cells["B" + row].GetValue<string>(),
+				Property = new Element(sheet.Cells["E" + row].GetValue<string>()),
+				Value = sheet.Cells["D" + row].GetValue<string>(),
+				Target = sheet.Cells["C" + row].GetValue<string>()
+			};
+
+			return testStep;
+		}
+	}
+}
---
+++ Remo/Descriptor/TestCase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OpenQA.Selenium;
+
+namespace Remo
+{
+	/// <summary>
+	/// Represent single complete test case
+	/// </summary>
+	public class TestCase
+	{
+		private IList<TestStep> steps;
+		
+		public TestCase()
+		{
+			steps = new List<TestStep>();
+		}
+
+		public int Steps
+		{
+			get { return steps.Count; }
+		}
+
+		/// <summary>
+		/// Adding test step to form a complete test case
+		/// </summary>
+		/// <param name="testStep"></param>
+		/// <returns></returns>
+		public TestCase Add(TestStep testStep)
+		{
+			this.steps.Add(testStep);
+			return this;
+		}
+
+		public TestResult Result { get; private set; }
+
+		public void Run(IWebDriver driver)
+		{
+			Result = TestResult.NoRun;
+
+			for (int i = 0; i < Steps; i++)
+			{
+				try
+				{
+					CommandFactory.Activate(this.steps[i]).SetDriver(driver).Execute();
+				}
+				catch(Exception e)
+				{
+					Debug.Print(e.Message);
+					Result = TestResult.Failed;
+					return;
+				}
+			}
+
+			Result = TestResult.Passed;
+		}
+
+	}
+
+	public enum TestResult
+	{
+		NoRun,
+		Failed,
+		Passed
+	}
+}
